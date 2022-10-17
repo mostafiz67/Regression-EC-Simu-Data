bcc991ca9f43acc69d81d1562596ba065296b8cd
mostafiz67 <mostafiz67.cu.cse@gmail.com>
2022-10-16 21:27:30 -0300

    Added figs without Uniform+-

--- src/accuracy_plot.py
+++ src/accuracy_plot.py
@@ -10,12 +10,12 @@ PLOT_OUTPUT_PATH = os.path.join(FIG_FOLDER_ROOT, "accuracy_vs_ec_plot/")
 
 
 CSVS = [
-    OUTPUT_FILE_ROOT / "output" / "Heteroscedastic_error.csv",
-    OUTPUT_FILE_ROOT / "output" / "Outliers_error.csv",
+    # OUTPUT_FILE_ROOT / "output" / "Heteroscedastic_error.csv",
+    # OUTPUT_FILE_ROOT / "output" / "Outliers_error.csv",
     OUTPUT_FILE_ROOT / "output" / "Norm_error.csv",
     OUTPUT_FILE_ROOT / "output" / "Uniform+_error.csv",
     OUTPUT_FILE_ROOT / "output" / "Uniform-_error.csv",
-    OUTPUT_FILE_ROOT / "output" / "Uniform+-_error.csv",
+    # OUTPUT_FILE_ROOT / "output" / "Uniform+-_error.csv",
 ]
 
 # create a single dataframe
@@ -25,14 +25,16 @@ df['Dataset'].replace(regex=True,inplace=True,to_replace='_error',value=r'')
 def ec_vs_accuracy():
     for method in df.Method.unique():
         data = df[df.Method.eq(method)]
-        plt.figure(figsize=(10, 6))
-        sns.scatterplot(data=data, x="EC", y="MAE", hue="Dataset", style="Regressor")
-        plt.legend(bbox_to_anchor=(1.01, 1), borderaxespad=0)
-        plt.xlabel("Error Consistency (EC)")
-        plt.ylabel("Mean Absolute Error (MAE)")
-        plt.title(f"Error Consistency ({method}) vs Mean Absolute Error (MAE)")
-        plt.legend(loc="upper left")
-        plt.savefig(PLOT_OUTPUT_PATH + f"{method}_MAE.png")
+        plt.figure(figsize=(15, 15))
+        sns.scatterplot(data=data, x="EC", y="MAE", hue="Dataset", style="Regressor", s=250)
+        plt.legend(bbox_to_anchor=(1, 0.5), borderaxespad=0, fontsize=20, markerscale=3, loc="upper left")
+        plt.xlabel("Error Consistency (EC)", fontsize=22)
+        plt.ylabel("Mean Absolute Error (MAE)", fontsize=22)
+        plt.xticks(fontsize=20)
+        plt.yticks(fontsize=20)
+        plt.title(f"Error Consistency ({method}) vs Mean Absolute Error (MAE)", fontsize=22)
+        # plt.legend(loc="center left")
+        plt.savefig(PLOT_OUTPUT_PATH + f"{method}_MAE_without_Uniform+-.png", bbox_inches='tight')
         plt.clf()
 
 def ec_vs_accuracy_dataset():
@@ -41,17 +43,19 @@ def ec_vs_accuracy_dataset():
         data = df[df["Dataset"] == dataset]
         # print(data)
         data = data.drop(data[(data.Method == "intersection_union_distance")].index) # droping distance for better view
-        plt.figure(figsize=(10, 8))
+        plt.figure(figsize=(12, 12))
         sns.scatterplot(data=data, x="EC", y="MAE", hue="Method", style="Regressor", s=100)
-        plt.legend(bbox_to_anchor=(1, 0.5), borderaxespad=0, loc="center left")
-        plt.xlabel("Error Consistency (EC)")
-        plt.ylabel("Mean Absolute Error (MAE)")
-        plt.title(f"Error Consistency vs MAE for the {dataset} dataset ")
+        plt.legend(bbox_to_anchor=(1, 0.5), borderaxespad=0, loc="center left", fontsize=20, markerscale=3)
+        plt.xlabel("Error Consistency (EC)", fontsize=22)
+        plt.ylabel("Mean Absolute Error (MAE)", fontsize=22)
+        plt.xticks(fontsize=20)
+        plt.yticks(fontsize=20)
+        plt.title(f"Error Consistency vs MAE for the {dataset} dataset ", fontsize=22)
         plt.savefig(PLOT_OUTPUT_PATH + f"MAE_{dataset}_only_without_distance.png", bbox_inches='tight')
         plt.clf()
 
 
 
 if __name__ == "__main__":
-    # ec_vs_accuracy()
-    ec_vs_accuracy_dataset()
+    ec_vs_accuracy()
+    # ec_vs_accuracy_dataset()
